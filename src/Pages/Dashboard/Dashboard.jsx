14186845193c0086e89e43fb7e35e60d35e38960
miamiral <miralmiaalexandra07@gmail.com>
2023-03-17 11:54:12 +0800

    update nako ang dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Dashboard.css'
 
-const Dashboard = () => {
+function Dashboard() {
   return (
-    <div>YOW NAKA LOGIN NAKA DUDE</div>
+    <>
+        <nav classname='Dashboard'>
+          <div classname='Dashboard-container'>
+              <button to="/" classname='dash-logo'>
+                  BIMS <i className="fa-sharp fa-solid fa-house"></i>
+              </button>
+              <button className='addresident-icon'>
+                 <i class="fa-sharp fa-solid fa-user-plus"></i>
+              </button>
+              <button className='household-icon'>
+                  <i class="fa-sharp fa-solid fa-users"></i>
+              </button>
+              <button className='graph-icon'>
+                 <i class="fa-sharp fa-solid fa-chart-simple"></i>
+              </button>
+              <button className='maps-icon'>
+                 <i class="fa-sharp fa-solid fa-map"></i>
+              </button>
+              <button className='user-icon'>
+                  <i class="fa-sharp fa-solid fa-user"></i>
+              </button>
+          </div>
+        </nav>
+    </>
   )
 }
 
