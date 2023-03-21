83a5b0db07f779cb62a385142e436c604d6ffea1
ikemanuuel <darkibahay@gmail.com>
2023-03-21 14:51:29 +0800

    update

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState}from 'react'
 import { 
     BsHouseFill,
     BsFillPersonFill,
@@ -7,12 +7,16 @@ import {
 import { RxDashboard } from "react-icons/rx";
 import { TbReport } from "react-icons/tb";
 import { FaBars } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
+import './sidebar.css';
 
-const Sidebar = () => {
-  const menuItem=[
+const Sidebar = ({children}) => {
+   const[isOpen ,setIsOpen] = useState(false);
+   const toggle = () => setIsOpen (!isOpen);
+   const menuItem=[
              {
                 path:"/",
-                name:"dashboard",
+                name:"Dashboard",
                 icon:< RxDashboard/>
              },
              {
@@ -32,17 +36,25 @@ const Sidebar = () => {
              },
     ]
     return (
-
-        <div className="sidebarcontain">
-            <div className="sidebar">
-               <div className="top_section">
-                  <h1 className="logo">logo</h1>
-                  <div className="bars">
-                        <FaBars/>
-                  </div>
-               </div>
-            </div>
-        </div> 
+      <div className="container">
+         <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
+             <div className="top_section">
+                 <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
+                 <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
+                     <FaBars onClick={toggle}/>
+                 </div>
+             </div>
+             {
+                 menuItem.map((item, index)=>(
+                     <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                         <div className="icon">{item.icon}</div>
+                         <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                     </NavLink>
+                 ))
+             }
+         </div>
+         <main>{children}</main>
+      </div>
          
   
     )
